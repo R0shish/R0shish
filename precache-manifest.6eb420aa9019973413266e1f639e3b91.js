15120b68c3e0a5faaf2e92d44f00fefe70224988
Roshish Shrestha <roshish@Roshishs-MacBook-Air.local>
2022-03-13 21:21:59 +0545

    Updates

---
+++ precache-manifest.6eb420aa9019973413266e1f639e3b91.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27655c800d5bfdc3b5db861ecd7acb8a",
+    "url": "/personal-website/index.html"
+  },
+  {
+    "revision": "de87b80896f931c3ce34",
+    "url": "/personal-website/static/css/main.4a35b45f.chunk.css"
+  },
+  {
+    "revision": "bd7270912d8ad81ece7a",
+    "url": "/personal-website/static/js/2.ab85592f.chunk.js"
+  },
+  {
+    "revision": "8d6ec8aa5e011f3a2f164cb9de3e4309",
+    "url": "/personal-website/static/js/2.ab85592f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de87b80896f931c3ce34",
+    "url": "/personal-website/static/js/main.9c9546d9.chunk.js"
+  },
+  {
+    "revision": "0a6fe392701c4ea4ead1",
+    "url": "/personal-website/static/js/runtime-main.99095661.js"
+  },
+  {
+    "revision": "2e2ee372a101079989524b92bbd4dbf4",
+    "url": "/personal-website/static/media/AEtest.2e2ee372.mp4"
+  },
+  {
+    "revision": "c72bed136e341f4c80a741fd983a1636",
+    "url": "/personal-website/static/media/background1080.c72bed13.jpg"
+  },
+  {
+    "revision": "01b25038d576a914e7f3d8bdb2d7aaea",
+    "url": "/personal-website/static/media/bg4.01b25038.jpg"
+  },
+  {
+    "revision": "56e1a61bf474b40478ac92263a71b140",
+    "url": "/personal-website/static/media/work.56e1a61b.jpg"
+  }
+]);
